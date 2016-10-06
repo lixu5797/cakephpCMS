dc8839c1f7abbea4651971b75cd07ea8b0eae608
idea-PC\ri.kyoku <ri.kyoku@seattleconsulting.co.jp>
2016-10-06 17:01:03 +0900

    スケジュールモデル修正

--- app/Model/Schedule.php
+++ app/Model/Schedule.php
@@ -7,12 +7,19 @@ App::uses('AppModel', 'Model');
 class Schedule extends AppModel {
 
 /**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+/**
  * Validation rules
  *
  * @var array
  */
 	public $validate = array(
-		'start_time' => array(
+		'title' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
 				//'message' => 'Your custom message here',
@@ -22,9 +29,29 @@ class Schedule extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'name' => array(
+			'maxLength' => array(
+				'rule' => array('maxLength'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'start_time' => array(
+			'datetime' => array(
+				'rule' => array('datetime'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
 		'end_time' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
+			'datetime' => array(
+				'rule' => array('datetime'),
 				//'message' => 'Your custom message here',
 				//'allowEmpty' => false,
 				//'required' => false,
