645070400e9ab25f7b54cee2ff296c774a5cb4b1
idea-PC\ri.kyoku <ri.kyoku@seattleconsulting.co.jp>
2016-10-06 16:46:25 +0900

    セッション、スケジュールモデルの作成

---
+++ app/Model/Schedule.php
@@ -0,0 +1,36 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Schedule Model
+ *
+ */
+class Schedule extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'start_time' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'end_time' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
--- app/View/Elements/Layouts/navbar_logined.tpl
+++ app/View/Elements/Layouts/navbar_logined.tpl
@@ -11,6 +11,7 @@
 				<li class="active"><a href="#">Home</a></li>
 				<li><a href="#about">About</a></li>
 				<li><a href="#contact">Contact</a></li>
+				<li><a href="{$this->base}/admin/schedules">Schedule</a></li>
 			</ul>
 			<ul class="nav pull-right">
 				<li>
